79cebcaf054189b9fbb66b25a76b0e5f6ccab78f
jambalaya555 <jambalaya555@hotmail.com>
2019-05-26 18:33:22 +0200

    Added timing

--- TestCiphers.py
+++ TestCiphers.py
@@ -1,13 +1,13 @@
 from AESCipher import AESCipher
 from DES3Cipher import DES3Cipher
-import time
+import time, os
 
 cipherEncFunctions = {
-    0: lambda cipher: cipher.encrypt('Testing CBC', 'CBC'),
-    1: lambda cipher: cipher.encrypt('Testing ECB', 'ECB'),
-    2: lambda cipher: cipher.encrypt('Testing CFB', 'CFB'),
-    3: lambda cipher: cipher.encrypt('Testing OFB', 'OFB'),
-    4: lambda cipher: cipher.encrypt('Testing CTR', 'CTR'),
+    0: lambda cipher, plaintext: cipher.encrypt(plaintext, 'CBC'),
+    1: lambda cipher, plaintext: cipher.encrypt(plaintext, 'ECB'),
+    2: lambda cipher, plaintext: cipher.encrypt(plaintext, 'CFB'),
+    3: lambda cipher, plaintext: cipher.encrypt(plaintext, 'OFB'),
+    4: lambda cipher, plaintext: cipher.encrypt(plaintext, 'CTR'),
 }
 cipherDecFunctions = {
     0: lambda cipher, cipherText: cipher.decrypt(cipherText, 'CBC'),
@@ -17,14 +17,25 @@ cipherDecFunctions = {
     4: lambda cipher, cipherText: cipher.decrypt(cipherText, 'CTR'),
 }
 
+def openTestingFiles():
+    global smallFile
+    global mediumFile
+    global largeFile
+    global rawOutputFile
+
+    smallFile = open(os.path.join(os.path.dirname(__file__), "testdata/1MB/dummy.txt"), "r")
+    mediumFile = open(os.path.join(os.path.dirname(__file__), "testdata/128MB/dummy.txt"), "r")
+    largeFile = open(os.path.join(os.path.dirname(__file__), "testdata/1GB/dummy.txt"), "r")
+    rawOutputFile = open(os.path.join(os.path.dirname(__file__), "output/raw.txt"), "w")
+
+
 def testAES():
     aesCipher = AESCipher('aaaaaaqweqweqweasdawqawqswqawews') # 32 bit key
     i = 0
     while i < 5:
         t = time.perf_counter()
-        cipher = cipherEncFunctions[i](aesCipher)
-        print(cipher)
-        print(cipherDecFunctions[i](aesCipher, cipher))
+        cipher = cipherEncFunctions[i](aesCipher, smallFile.read()) # encrypt the file
+        cipherDecFunctions[i](aesCipher, cipher) # decrypt the file
         print("Time elapsed: " + str(time.perf_counter() - t))
         i+=1
 
@@ -32,10 +43,12 @@ def testDES3():
     des3Cipher = DES3Cipher('aaaaaaqweqweqwea') # 16 bit key
     i = 0
     while i < 5:
-        cipher = cipherEncFunctions[i](des3Cipher)
-        print(cipher)
-        print(cipherDecFunctions[i](des3Cipher, cipher))
+        t = time.perf_counter()
+        cipher = cipherEncFunctions[i](des3Cipher, smallFile.read()) # encrypt the file
+        cipherDecFunctions[i](des3Cipher, cipher) # decrypt the file
+        print("Time elapsed: " + str(time.perf_counter() - t))
         i+=1
 
+openTestingFiles()
 testAES()
 testDES3()
